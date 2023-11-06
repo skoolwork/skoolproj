7d6c08bcf75531478216a57b9787c5698926e95e
skoolwork <149764746+skoolwork@users.noreply.github.com>
2023-11-06 14:09:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import pygame,sys,pymunk
 def create_blocks(space):
     body=pygame.body(1,100,body_type=pymunk.Body.DYNAMIC)
     body.position = (300,200)
-    shape=pymunk.Poly
+    shape=pymunk.Circle(body,10)
+    space.add(body.shape)
+    return shape
+
+def draw_blocks(blocks):
+    for blocks in blocks:
+        pygame.draw.circle(screen(0,0,0),blocks.body.position,10)
 
 pygame.init()
 screen=pygame.display.set_mode((600,400))
 clock=pygame.time.Clock()
-space=pymunk.space()
+space=pymunk.Space()
 space.gravity = (0,500)
 
 while True:
